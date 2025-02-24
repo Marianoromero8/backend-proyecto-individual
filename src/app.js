10a1e5235260e3a14749bf7f228fe75ddb7c76d0
Marianoromero8 <marianoromero97.mr@gmail.com>
2025-02-23 23:51:39 -0300

    Only one value for CORS

--- src/app.js
+++ src/app.js
@@ -15,9 +15,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', [
-    'http://localhost:3000', //Local enviroment
-    'https://backend-proyecto-individual.onrender.com']); // Render
+  res.header('Access-Control-Allow-Origin', 'https://backend-proyecto-individual.onrender.com'); // Render
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
